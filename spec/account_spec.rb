ef52421f0432f68ab7f0ef4fc9a968e5021baf97
Kim Wilson <k.wilson541@gmail.com>
2017-01-23 10:22:51 +0000

    Refactored test to make explicit subject

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 
 describe Account do
 
+	subject(:account) { described_class.new }
+
 	context 'creating an account' do
 
 		it 'creates a new account with zero balance' do
-			account = Account.new
 			expect(account.check_balance).to eq 0
 		end
-
 	end
-
 end
