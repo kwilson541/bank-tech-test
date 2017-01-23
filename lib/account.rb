2e64a5f48796a7341099512fb06b8505c6b95712
Kim Wilson <k.wilson541@gmail.com>
2017-01-23 11:46:57 +0000

    Account is created with an empty transaction log, test passing

--- lib/account.rb
+++ lib/account.rb
@@ -2,10 +2,13 @@ class Account
 
 	def initialize
 		@balance = OPENING_BALANCE
+		@transaction_log = []
+		new_transaction(nil, nil)
 	end
 
 	def check_balance
-		@balance
+		return "£%.2f" % @balance if @balance >= OPENING_BALANCE
+		"-£%.2f" % @balance.abs
 	end
 
 	def deposit(amount)
@@ -16,10 +19,48 @@ class Account
 		@balance -= amount
 	end
 
+	def print_statement
+		generate_statement
+	end
+
 	private
 
 		OPENING_BALANCE = 0
+		COLUMN_WIDTH = 10
 
 		attr_reader :balance
+
+		def new_transaction(type, amount)
+			credit = ""
+			debit = ""
+			credit = amount if type == "credit"
+			debit = amount if type == "debit"
+			@transaction_log << { date: get_date, credit: credit, debit: debit, balance: check_balance }
+		end
+
+		def get_date
+			Time.now.strftime("%d/%m/%Y")
+		end
+
+		def generate_statement
+			statement = statement_header
+			@transaction_log.each { |transaction|
+				statement += "#{transaction[:date]}|"\
+							"#{format_column(transaction[:credit])}|"\
+							"#{format_column(transaction[:debit])}|"\
+							"#{format_column(transaction[:balance])}\n"
+			}
+			statement
+		end
+
+		def statement_header
+			"#{format_column("date")}|#{format_column("credit")}|"\
+			"#{format_column("debit")}|#{format_column("balance")}\n"
+		end
+
+		def format_column(data)
+			whitespace = COLUMN_WIDTH - data.length if data.length < COLUMN_WIDTH
+			data += " " * whitespace 
+		end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,7 +7,7 @@ describe Account do
 	context 'creating an account' do
 
 		it 'creates a new account with zero balance' do
-			expect(account.check_balance).to eq 0
+			expect(account.check_balance).to eq "£0.00"
 		end
 	end
 
@@ -15,12 +15,19 @@ describe Account do
 
 		it 'allows a user to deposit money' do
 			account.deposit(100.00)
-			expect(account.check_balance).to eq 100.00
+			expect(account.check_balance).to eq "£100.00"
 		end
 
 		it 'allows a user to withdraw money' do
 			account.withdraw(100.00)
-			expect(account.check_balance).to eq -100.00
+			expect(account.check_balance).to eq "-£100.00"
+		end
+	end
+
+	context 'logging transactions' do
+
+		it 'has an empty log on new accounts' do
+			expect(account.print_statement).to eq "date      |credit    |debit     |balance   \n23/01/2017|          |          |£0.00     \n"
 		end
 	end
 end
